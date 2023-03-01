1338029703f99fd839d0787cd71afcc9155edfae
Samuel Martín <alu0101359526@ull.edu.es>
2023-03-01 12:58:38 +0000

    Implemented the last part of the project

--- matrix_t.hpp
+++ matrix_t.hpp
@@ -192,5 +192,12 @@ template<class T>
 void
 matrix_t<T>::multiply(const matrix_t<T>& A, const matrix_t<T>& B)
 {
-  // rellenar código
+assert(A.get_n() == B.get_m());
+  resize(A.get_m(), B.get_n());
+  for (int i = 1; i <= get_m(); ++i)
+    for (int j = 1; j <= get_n(); ++j) {
+      at(i, j) = 0;
+      for (int k = 1; k <= A.get_n(); ++k)
+        at(i, j) = at(i, j) + A(i, k) * B(k, j);
+    }
 }
